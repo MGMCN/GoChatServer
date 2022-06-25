a578ea8bb1667a94f347265807a99299d5103993
gaoshan <takayamachina@gmail.com>
2022-06-26 00:20:06 +0900

    update proj

--- GoChat/Client/goclient.go
+++ GoChat/Client/goclient.go
@@ -1,3 +1,28 @@
 package Client
 
-// 目前这个程序太简单了，就没有单独写一个client...
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+var (
+	done = make(chan struct{})
+)
+
+func WaitDone(){
+	<- done
+}
+
+func TerminalOutput(conn net.Conn) {
+	io.Copy(os.Stdout,conn)
+	log.Println("done")
+	done <- struct{}{}
+}
+
+func MustCopy(dst io.Writer, src io.Reader) {
+	if _, err := io.Copy(dst, src); err != nil {
+		log.Fatal(err)
+	}
+}
